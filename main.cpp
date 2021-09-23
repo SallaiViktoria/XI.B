7bb386a58089d0703a38eeb86d16f0bf83aee1f5
SallaiViktoria <91243514+SallaiViktoria@users.noreply.github.com>
2021-09-23 09:46:41 +0300

    Matrix

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int v[100][100], n;
+    cin >> n;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout << "a[ "<< i<<" ] ["<< j << "] ";
+            cin >> a [i] [j];
+        }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout << a [i][j]<< " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
