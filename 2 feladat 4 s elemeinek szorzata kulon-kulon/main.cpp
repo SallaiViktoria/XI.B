2b128d2f224875e5c5c1b6c967099c82fedaec01
SallaiViktoria <91243514+SallaiViktoria@users.noreply.github.com>
2021-09-30 07:11:50 +0300

    Add files via upload

---
+++ 2 feladat 4 s elemeinek szorzata kulon-kulon/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int m[100][100], n, i,j, P1=1,P2=1,P3=1,P4=1;
+    cout << "n=";
+    cin >> n;
+    for (i=0;i<n; i++){
+        for (j=0; j<n; j++){
+            cin >> m[i][j];
+        }
+    }
+    for (i=0; i<n; i++){
+        for ( j=0; j<n;;j++){
+            if (i<j && i+j<n-1){
+                P1= m[i][j]*P1;
+            }
+            if ( i<j; && i+j>n-1){
+                P2= ,[i][j]*P2;
+            }
+            if ( i>j && i+j> n-1){
+                P3= m[i][j]*P3;
+            }
+            if ( i>j && i+j< n-1){
+                P4= m[i][j]*P4;
+            }
+        }
+    }
+    cout << P1 << endl;
+    cout << P2 << endl;
+    cout << P3 << endl;
+    cout << P4 << endl;
+    return 0;
+}
