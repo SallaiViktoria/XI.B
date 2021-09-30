3a12ab55798dc4a4bb5b5fc3e4c709a1a59b045e
SallaiViktoria <91243514+SallaiViktoria@users.noreply.github.com>
2021-09-30 07:44:44 +0300

    Add files via upload

---
+++ 23_2_feladat/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int i,j,n,Sm, m[100][100];
+ cout << "n=";
+ cin >> n;
+ for (i=0;i<n; i++){
+    for(j=0; j<n;j++){
+        if (i+j=n-1)
+            Sm=Sm+m[100][100]
+    }
+ }
+
+    return 0;
+}
