8111882f99eeee1a336a5334611c0791905ec98e
SallaiViktoria <91243514+SallaiViktoria@users.noreply.github.com>
2023-02-07 08:30:07 +0200

    potbac2022-3t-2f

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n][n];
+    string szo;
+    cout << "kerema beolvasando stringet";
+    getline (cin, szo);
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin >> a[i][j];
+    int elofordul=0;
+    size_t hely;
+    hely =  szo.find("r");
+    if (hely! = string::npos) {
+        szo.replace (hely, 1, "o");
+    }
+    while (hely! = string::npos) {
+        hely = szo.find ("r", hely+1);
+        if (hely!= string::npos) {
+            szo.replace (hely, 1, "o");
+
         }
     }
 
-    for(int i=0;i<n;i++){
-            cout << endl;
-        for(int j=0;j<n-1;j++){
-            if(i<=j){
-                    a[i][j]=a[i][j+1];
-            }
-            cout << a[i][j] << " ";
-        }
-    }
     return 0;
 }
