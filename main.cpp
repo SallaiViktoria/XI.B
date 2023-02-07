6ed75f4a91e02ddc8c889bfaf10f068f59357bb1
SallaiViktoria <91243514+SallaiViktoria@users.noreply.github.com>
2023-02-07 08:28:47 +0200

    bac2022-3t-3f

--- main.cpp
+++ main.cpp
@@ -4,35 +4,6 @@ using namespace std;
 
 int main()
 {
-    int i, j, m, n, s=0,a[100][100];
-    cout << "m=";
-    cin >>m;
-    cout << "n=";
-    cin >> n;
-    for(int i=1;i<=m;i++) {
-        for(j=1;j<=n;j++) {
-            cout<<"a["<<i<<"]["<<j<<"]=";
-            cin >> a;
-        }
-    }
-    for(int i=0;i<=m;i++) {
-        a[i][0]=11;
-        a[i][n+1]=11;
-        for(int j=0;j<=n;j++) {
-            a[0][j]=11;
-            a[m+1][j]=11;
-        }
-    }
-    for(i=1;i<=m;i++) {
-        for(j=1;j<=n;j++) {
-            int min=a[i-1][j];
-            if(a[i+1][j]<min) min=a[i+1][j];
-            if(a[i][j-1]<min) min=a[i][j-1];
-            if(a[i][j+1]<min) min=a[i][j+1];
-            if(a=[i][j]<min)
-                s=s+(min-a[i][j]);
-        }
-    }
-    cout<<s;
+    cout << "Hello world!" << endl;
     return 0;
 }
